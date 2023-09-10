3bb0470a335973cbb73a02cf2dd0fd06a4b1f7f4
KonstantinGood <laudanum1995@yandex.ru>
2023-09-10 13:31:36 +0300

    hw-16

--- api.js
+++ api.js
@@ -1,45 +1,90 @@
-export function getTodos() {
-    return fetch("https://wedev-api.sky.pro/api/v1/Good-Konstantin/comments", {
-  method: "GET"
-})
+export let token;
 
-// подписываемся на успешное завершение запроса с помощью then
-.then((response) => {
-  // Запускаем преобразовываем "сырые" данные от API в json формат
-  return response.json();
-});
+export const setToken = (newToken) => {
+  token = newToken;
 }
 
-export function postTodo ({name, text}) {
-     return fetch("https://wedev-api.sky.pro/api/v1/Good-Konstantin/comments", {
+export let UserName;
+
+export const setUserName = (newUserName) => {
+  UserName = newUserName;
+}
+
+
+export function getTodos() {
+  return fetch("https://wedev-api.sky.pro/api/v2/Konstantin-Good/comments", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+    .then((response) => {
+      return response.json();
+    });
+}
+
+export function postTodo({ name, text }) {
+  return fetch("https://wedev-api.sky.pro/api/v2/Konstantin-Good/comments", {
     method: "POST",
     body: JSON.stringify({
       name: name
-      .replaceAll("&", "&amp;")
-     .replaceAll("<", "&lt;")
-     .replaceAll(">", "&gt;")
-       .replaceAll('"', "&quot;"),
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;"),
       text: text
-      .replaceAll("&", "&amp;")
-     .replaceAll("<", "&lt;")
-     .replaceAll(">", "&gt;")
-     .replaceAll('"', "&quot;"),
-      forceError: true,
+        // .replace(`"${quote}"\n`, "")
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;"),
+      // forceError: true,
     }),
-    // JSON.stringifylikes:
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
   })
-  .then((response) => {
-    if (response.status === 201) {
+    .then((response) => {
+      if (response.status === 201) {
+        return response.json();
+      } else if (response.status === 400) {
+        alert('Имя и текст должны быть длиннее 3-х символов!');
+        return Promise.reject(new Error("Не верный пользовательский ввод"));
+      } else if (response.status === 500) {
+        return Promise.reject(new Error("Сервер упал"));
+      }
+    })
+}
+
+export function login({ login, password }) {
+  return fetch("https://wedev-api.sky.pro/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  })
+    .then((response) => {
+      if (response.status === 400) {
+        throw new Error("Не верный логин или пароль");
+      };
       return response.json();
-    }
-    else if (response.status === 400) {
-      alert('мало символов');
-      return Promise.reject(new Error("Не верный пользовательский ввод"));
-    }
-    else{
-      alert('что-то пошло не так');
-      return Promise.reject(new Error('Ошибка сервера!'));
-    }
+    })
+}
+
+export function UserRegistration({ login, name, password }) {
+  return fetch("https://wedev-api.sky.pro/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      name,
+      password,
+    }),
   })
-  
+    .then((response) => {
+      if (response.status === 400) {
+        throw new Error("Повторяется имя");
+      };
+      return response.json();
+    })
 }
